5f398e1445d99e37e39af6142e8fbb373456d324
Rene-acc <al066654@uacam.mx>
2024-05-26 03:50:23 -0600

    fix estatus filtro correción de estilos

--- public/scripts/filtroEstatusFechas.js
+++ public/scripts/filtroEstatusFechas.js
@@ -1,52 +1,62 @@
-document.getElementById('filtroFormEstatus').addEventListener('submit', function(event) {
+document.getElementById('filtroFormEstatus').addEventListener('submit', async function(event) {
     event.preventDefault();
 
     const desdeFecha = document.getElementById('input_desde_status').value;
     const hastaFecha = document.getElementById('input_hasta_status').value;
 
-    fetch('/filtroFechaEstatus', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ desdeFecha, hastaFecha }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Construir la tabla dinámicamente
-        const newTable = construirTablaDinamica(data);
-        
-        // Reemplazar la tabla antigua con la nueva
-        const oldTable = document.getElementById('estatusTabla');
-        oldTable.parentNode.replaceChild(newTable, oldTable);
-        
-        // Llamar a la función para activar los estilos de la tabla
-        aplicarEstilosTabla(newTable);
-    })
-    .catch(error => console.error('Error:', error));
+    try {
+        const response = await fetch('/filtroFechaEstatus', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ desdeFecha, hastaFecha }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Error al filtrar datos');
+        }
+
+        const data = await response.json();
+        construirYActualizarTabla(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
+
+function construirYActualizarTabla(data) {
+    // Construir la tabla dinámicamente
+    const newTable = construirTablaDinamica(data);
+
+    // Obtener el contenedor actual de la tabla
+    const tablaContainer = document.getElementById('tablaContainer');
+
+    // Limpiar el contenedor antes de agregar la nueva tabla
+    tablaContainer.innerHTML = '';
+
+    // Agregar la nueva tabla al contenedor
+    tablaContainer.appendChild(newTable);
+}
 
 function construirTablaDinamica(data) {
     const { abierto, asignada, pendiente, espera, cerrado } = data;
-        const tableBody = document.querySelector('#estatusTabla tbody');
+    const newTable = document.createElement('table');
+    newTable.className = 'table'; // Clase principal
+    newTable.id = 'estatusTabla'; // ID principal
 
-        // Eliminar contenido anterior de la tabla
-        tableBody.innerHTML = '';
+    // Para cancelar --bs-table-bg: transparent;
+    newTable.style.setProperty('--bs-table-bg', 'aliceblue', 'important');
 
-        // Construir nueva tabla con encabezados y atributos
-        const newTable = document.createElement('table');
-        newTable.className = 'table'; // Clase principal
-        newTable.id = 'estatusTabla'; // ID principal
-
-        // Fila de encabezados principales
-        const headerRowMain = document.createElement('tr');
-        headerRowMain.className = 'tableEstatus'; // Clase de fila principal
+    // Fila de encabezados principales
+    const headerRowMain = document.createElement('tr');
+    headerRowMain.className = 'tableEstatus'; // Clase de fila principal
 
         // Abierto
         const abiertoHeader = document.createElement('th');
         abiertoHeader.setAttribute('colspan', '5');
         abiertoHeader.className = 'tablaTitulos'; // Clase de encabezado
         abiertoHeader.textContent = 'Abierto';
+        abiertoHeader.style.padding = '8px'; // Agregar padding de 8px
         headerRowMain.appendChild(abiertoHeader);
 
         // Asignadas
@@ -85,45 +95,48 @@ function construirTablaDinamica(data) {
 
         // Abierto
         headerRowColumns.innerHTML = `
-            <th>Folio</th>
-            <th>Nombre</th>
-            <th>Equipo</th>
-            <th>Fecha</th>
-            <th>Estatus</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Folio</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Nombre</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Equipo</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Fecha</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;" id="bordeD">Estatus</th>
         `;
 
         // Asignadas
         headerRowColumns.innerHTML += `
-            <th>Folio</th>
-            <th>Nombre</th>
-            <th>Técnico</th>
-            <th>Estatus</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Folio</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Nombre</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Técnico</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;" id="bordeDer">Estatus</th>
         `;
 
         // En Proceso
         headerRowColumns.innerHTML += `
-            <th>Folio</th>
-            <th>Nombre</th>
-            <th>Equipo</th>
-            <th>Estatus</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Folio</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Nombre</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Equipo</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;" id="bordeDer">Estatus</th>
         `;
 
         // En Espera
         headerRowColumns.innerHTML += `
-            <th>Folio</th>
-            <th>Nombre</th>
-            <th>Equipo</th>
-            <th>Estatus</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Folio</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Nombre</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Equipo</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;" id="bordeDer">Estatus</th>
         `;
 
         // Cerrado
         headerRowColumns.innerHTML += `
-            <th>Folio</th>
-            <th>Nombre</th>
-            <th>Equipo</th>
-            <th>Estatus</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Folio</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Nombre</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;">Equipo</th>
+        <th style="padding: 8px; background-color: #e2e3e5; color: rgb(0, 0, 0, 0.6) !important;" id="bordeDer">Estatus</th>
         `;
 
+
+
+        newTable.appendChild(headerRowMain);
         newTable.appendChild(headerRowColumns);
 
         // Cuerpo de la tabla
@@ -135,12 +148,12 @@ function construirTablaDinamica(data) {
             // Abierto
             if (i < abierto.length) {
                 row.innerHTML += `
-                    <td class="bg-danger">${abierto[i].FolioSolicitud}</td>
+                    <td class="bg-danger" style="padding: 8px;">${abierto[i].FolioSolicitud}</td>
                     <td class="bg-danger">${abierto[i].Nombre}</td>
                     <td class="bg-danger">${abierto[i].Equipo}</td>
                     <td class="bg-danger">${abierto[i].Fecha}</td>
-                    <td class="bg-danger">
-                        <select class="form-select cambiarEstado" data-folio="${abierto[i].FolioSolicitud}">
+                    <td class="seleccion bg-danger" id="bordeDerecho<%= i %>">
+                        <select class="form-select cambiarEstado" data-folio="${abierto[i].FolioSolicitud}" style="padding: .375rem 2.0rem .375rem .40rem;">
                             <option value="Abierto" selected>Abierto</option>
                             <option value="Proceso">En Proceso</option>
                             <option value="Espera">En Espera</option>
@@ -149,7 +162,7 @@ function construirTablaDinamica(data) {
                     </td>
                 `;
             } else {
-                row.innerHTML += '<td></td><td></td><td></td><td></td><td></td>';
+                row.innerHTML += '<td></td><td></td><td></td><td></td><td id="bordeDerechoVacio"></td>';
             }
 
             // Asignadas
@@ -158,8 +171,8 @@ function construirTablaDinamica(data) {
                     <td class="bg-warning">${asignada[i].FolioSolicitud}</td>
                     <td class="bg-warning">${asignada[i].UsuarioNombre}</td>
                     <td class="bg-warning">${asignada[i].Nombre}</td>
-                    <td class="bg-warning">
-                        <select class="form-select cambiarEstado" data-folio="${asignada[i].FolioSolicitud}">
+                    <td class="seleccion bg-warning" id="ladoDerecho<%= i %>" >
+                        <select class="form-select cambiarEstado" data-folio="${asignada[i].FolioSolicitud}" style="padding: .375rem 2.0rem .375rem .40rem;">
                             <option value="Asignada" selected>Asignada</option>
                             <option value="Proceso">En Proceso</option>
                             <option value="Espera">En Espera</option>
@@ -168,7 +181,7 @@ function construirTablaDinamica(data) {
                     </td>
                 `;
             } else {
-                row.innerHTML += '<td></td><td></td><td></td><td></td>';
+                row.innerHTML += '<td></td><td></td><td></td><td id="ladoDerechoVacio"></td>';
             }
 
             // En Proceso
@@ -177,8 +190,8 @@ function construirTablaDinamica(data) {
                     <td class="bg-info">${pendiente[i].FolioSolicitud}</td>
                     <td class="bg-info">${pendiente[i].Nombre}</td>
                     <td class="bg-info">${pendiente[i].Equipo}</td>
-                    <td class="bg-info">
-                        <select class="form-select cambiarEstado" data-folio="${pendiente[i].FolioSolicitud}">
+                    <td class="seleccion bg-info" id="pendienteDerecha<%= i %>">
+                        <select class="form-select cambiarEstado" data-folio="${pendiente[i].FolioSolicitud}" style="padding: .375rem 2.0rem .375rem .40rem;">
                             <option value="Proceso" selected>En Proceso</option>
                             <option value="Abierto">Abierto</option>
                             <option value="Espera">En Espera</option>
@@ -187,7 +200,7 @@ function construirTablaDinamica(data) {
                     </td>
                 `;
             } else {
-                row.innerHTML += '<td></td><td></td><td></td><td></td>';
+                row.innerHTML += '<td></td><td></td><td></td><td id="pendienteVacioDerecha"></td>';
             }
 
             // En Espera
@@ -196,8 +209,8 @@ function construirTablaDinamica(data) {
                     <td class="bg-primary">${espera[i].FolioSolicitud}</td>
                     <td class="bg-primary">${espera[i].Nombre}</td>
                     <td class="bg-primary">${espera[i].Equipo}</td>
-                    <td class="bg-primary">
-                        <select class="form-select cambiarEstado" data-folio="${espera[i].FolioSolicitud}">
+                    <td class="seleccion bg-primary" id="esperarDerecha<%= i %>">
+                        <select class="form-select cambiarEstado" data-folio="${espera[i].FolioSolicitud}" style="padding: .375rem 2.0rem .375rem .40rem;">
                             <option value="Proceso">En Proceso</option>
                             <option value="Abierto">Abierto</option>
                             <option value="Espera" selected>En Espera</option>
@@ -206,7 +219,7 @@ function construirTablaDinamica(data) {
                     </td>
                 `;
             } else {
-                row.innerHTML += '<td></td><td></td><td></td><td></td>';
+                row.innerHTML += '<td></td><td></td><td></td><td id="esperaVacioDerecha"></td>';
             }
 
             // Cerrado
@@ -215,8 +228,8 @@ function construirTablaDinamica(data) {
                     <td class="bg-success">${cerrado[i].FolioSolicitud}</td>
                     <td class="bg-success">${cerrado[i].Nombre}</td>
                     <td class="bg-success">${cerrado[i].Equipo}</td>
-                    <td class="bg-success">
-                        <select class="form-select cambiarEstado" data-folio="${cerrado[i].FolioSolicitud}">
+                    <td class="seleccion bg-success" id="ladoDerecho<%= i %>" >
+                        <select class="form-select cambiarEstado" data-folio="${cerrado[i].FolioSolicitud}" style="padding: .375rem 2.0rem .375rem .40rem;">
                             <option value="Cerrado" selected>Cerrado</option>
                             <option value="Proceso">En Proceso</option>
                             <option value="Espera">En Espera</option>
@@ -225,7 +238,7 @@ function construirTablaDinamica(data) {
                     </td>
                 `;
             } else {
-                row.innerHTML += '<td></td><td></td><td></td><td></td>';
+                row.innerHTML += '<td></td><td></td><td></td>';
             }
 
             newTable.appendChild(row);
@@ -234,60 +247,7 @@ function construirTablaDinamica(data) {
         const oldTable = document.getElementById('estatusTabla');
         oldTable.parentNode.replaceChild(newTable, oldTable);
         // Llamar a la función para activar los estilos de la tabla
-        reactivarEstilosTabla();
+        
 
     return newTable;
-}
-
-function aplicarEstilosTabla(table) {
-    try {
-        // Seleccionar la tabla y sus elementos hijos
-        var tabla = table;
-        if (tabla) {
-            // Aplicar estilos CSS a la tabla y sus elementos
-            tabla.classList.add("table", "table-striped", "table-bordered");
-
-            // Iterar sobre las filas de la tabla (excepto la primera que es el encabezado)
-            var filas = tabla.getElementsByTagName("tr");
-            for (var i = 0; i < filas.length; i++) {
-                var fila = filas[i];
-
-                // Aplicar estilos a las filas dependiendo de su clase
-                if (fila.classList.contains("table-secondary")) {
-                    fila.classList.add("table-secondary");
-                } else if (fila.classList.contains("table-light")) {
-                    fila.classList.add("table-light");
-                }
-
-                // Iterar sobre las celdas de cada fila para aplicar estilos
-                var celdas = fila.getElementsByTagName("td");
-                for (var j = 0; j < celdas.length; j++) {
-                    var celda = celdas[j];
-
-                    // Aplicar clases de Bootstrap o estilos personalizados según sea necesario
-                    if (celda.classList.contains("bg-danger")) {
-                        celda.classList.add("bg-danger");
-                    } else if (celda.classList.contains("bg-warning")) {
-                        celda.classList.add("bg-warning");
-                    } else if (celda.classList.contains("bg-info")) {
-                        celda.classList.add("bg-info");
-                    } else if (celda.classList.contains("bg-primary")) {
-                        celda.classList.add("bg-primary");
-                    } else if (celda.classList.contains("bg-success")) {
-                        celda.classList.add("bg-success");
-                    } else if (celda.classList.contains("seleccion")) {
-                        celda.classList.add("seleccion");
-                    }
-                }
-
-                // Aplicar estilos a los select dentro de las celdas
-                var selects = fila.querySelectorAll("select.form-select.cambiarEstado");
-                for (var k = 0; k < selects.length; k++) {
-                    selects[k].classList.add("form-select", "cambiarEstado");
-                }
-            }
-        }
-    } catch (error) {
-        console.error("Error al reactivar los estilos de la tabla:", error);
-    }
 }
